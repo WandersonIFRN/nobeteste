9f014cfc7ff181776da86941315d28a0ec37a00c
Wanderson <wanderson_costa01@hotmail.com>
2022-02-17 07:55:51 -0300

    teste heroku sem conf

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :register, dependent: :destroy
   # Include default devise modules. Others available are:
-  #  :lockable, :timeoutable, :trackable and :omniauthable
-  devise :confirmable,:database_authenticatable, :registerable,
+  #  :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
   validates :saldo, numericality: {only_float: true, :greater_than => 0}
